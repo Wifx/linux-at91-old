7e0838b16f2339015b417cc312f7f9075a5a9808
Yannick Lanz <yannick.lanz@wifx.net>
2019-02-28 10:51:15 +0100

    Fixed files license

--- drivers/mfd/pmic-lorix.c
+++ drivers/mfd/pmic-lorix.c
@@ -1,3 +1,14 @@
+/*
+ * Driver to communicate with the LORIX One PMIC/Reset controller
+ *
+ *  Copyright (C) 2016 Wifx,
+ *                2016 Yannick Lanz <yannick.lanz@wifx.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
